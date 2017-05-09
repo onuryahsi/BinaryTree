088c5778f70c39694abbdb1f41b484bd86be8a8a
Onur <onuryahsi@windowslive.com>
2017-05-09 11:34:10 +0300

    Add files via upload

---
+++ bTree.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "bTreeLib.h"
+
+int main(){
+	node* tree = NULL;
+	tree = insert(tree,02);
+	tree = insert(tree,280);
+	tree = insert(tree,190);
+	tree = insert(tree,213);
+	tree = insert(tree,56);
+	tree = insert(tree,24);
+	tree = insert(tree,18);
+	tree = insert(tree,27);
+	displayTree(tree);
+	return 0;
+}
